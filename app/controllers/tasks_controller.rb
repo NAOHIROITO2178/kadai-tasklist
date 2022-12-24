db5b554cad910509ae5cf36b9d09123fda84db19
naohiro.ito <shanghongyiteng429@gmail.com>
2022-12-24 13:52:23 +0000

    カラム追加による諸々の変更

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
     def index
         @tasks = Task.all
     end
@@ -28,6 +29,7 @@ class TasksController < ApplicationController
   end
 
   def update
+    binding.pry
     @task = Task.find(params[:id])
 
     if @task.update(task_params)
@@ -50,7 +52,12 @@ class TasksController < ApplicationController
   private
 
   # Strong Parameter
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end
